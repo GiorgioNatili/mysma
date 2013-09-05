f213cf3778e3b4b43245a2f7110870e2964bbd97
ArkeologeN <hamzawaqas.programmer@gmail.com>
2013-09-05 14:58:16 -0700

    blank opencart extended with public opencart-api

---
+++ admin/config.php
@@ -0,0 +1,30 @@
+<?php
+// HTTP
+define('HTTP_SERVER', 'http://localhost/mysma/admin/');
+define('HTTP_CATALOG', 'http://localhost/mysma/');
+
+// HTTPS
+define('HTTPS_SERVER', 'http://localhost/mysma/admin/');
+define('HTTPS_CATALOG', 'http://localhost/mysma/');
+
+// DIR
+define('DIR_APPLICATION', '/var/www/mysma/admin/');
+define('DIR_SYSTEM', '/var/www/mysma/system/');
+define('DIR_DATABASE', '/var/www/mysma/system/database/');
+define('DIR_LANGUAGE', '/var/www/mysma/admin/language/');
+define('DIR_TEMPLATE', '/var/www/mysma/admin/view/template/');
+define('DIR_CONFIG', '/var/www/mysma/system/config/');
+define('DIR_IMAGE', '/var/www/mysma/image/');
+define('DIR_CACHE', '/var/www/mysma/system/cache/');
+define('DIR_DOWNLOAD', '/var/www/mysma/download/');
+define('DIR_LOGS', '/var/www/mysma/system/logs/');
+define('DIR_CATALOG', '/var/www/mysma/catalog/');
+
+// DB
+define('DB_DRIVER', 'mysql');
+define('DB_HOSTNAME', 'localhost');
+define('DB_USERNAME', 'root');
+define('DB_PASSWORD', 'admin123');
+define('DB_DATABASE', 'mysma');
+define('DB_PREFIX', 'oc_');
+?>
---
+++ config.php
@@ -0,0 +1,27 @@
+<?php
+// HTTP
+define('HTTP_SERVER', 'http://localhost/mysma/');
+
+// HTTPS
+define('HTTPS_SERVER', 'http://localhost/mysma/');
+
+// DIR
+define('DIR_APPLICATION', '/var/www/mysma/catalog/');
+define('DIR_SYSTEM', '/var/www/mysma/system/');
+define('DIR_DATABASE', '/var/www/mysma/system/database/');
+define('DIR_LANGUAGE', '/var/www/mysma/catalog/language/');
+define('DIR_TEMPLATE', '/var/www/mysma/catalog/view/theme/');
+define('DIR_CONFIG', '/var/www/mysma/system/config/');
+define('DIR_IMAGE', '/var/www/mysma/image/');
+define('DIR_CACHE', '/var/www/mysma/system/cache/');
+define('DIR_DOWNLOAD', '/var/www/mysma/download/');
+define('DIR_LOGS', '/var/www/mysma/system/logs/');
+
+// DB
+define('DB_DRIVER', 'mysql');
+define('DB_HOSTNAME', 'localhost');
+define('DB_USERNAME', 'root');
+define('DB_PASSWORD', 'admin123');
+define('DB_DATABASE', 'mysma');
+define('DB_PREFIX', 'oc_');
+?>
